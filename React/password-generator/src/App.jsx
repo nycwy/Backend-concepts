4a04d0d8bdeddc9705ef523832e96f04530997fd
nycwy <stuxy.1129@gmail.com>
2025-12-07 23:38:24 +0530

    Random password generator project initialize

---
+++ React/password-generator/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-4xl text-center'>Password Generator</h2>
+    </>
+  )
+}
+
+export default App
