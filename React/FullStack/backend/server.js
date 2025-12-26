9ec8560141abb0fbfa5e90ca0ab0675a1d92d9fe
nycwy <stuxy.1129@gmail.com>
2025-12-26 21:50:35 +0530

    Backend

---
+++ React/FullStack/backend/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Server is ready");
+});
+
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: "Joke A",
+            content: "This is joke A"
+        },
+        {
+            id: 2,
+            title: "Joke B",
+            content: "This is joke B"
+        },
+        {
+            id: 3,
+            title: "Joke C",
+            content: "This is joke C"
+        },
+        {
+            id: 4,
+            title: "Joke D",
+            content: "This is joke D"
+        },
+    ]
+    res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost: ${port}`);
+});
